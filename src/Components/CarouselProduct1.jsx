d10b6a9ba3ca6c8be008d2c62c4877072e6d8ed3
gstasaitis <theqerd@gmail.com>
2024-02-20 18:26:15 +0200

    imgchange

--- src/Components/CarouselProduct1.jsx
+++ src/Components/CarouselProduct1.jsx
@@ -1,12 +1,11 @@
-const CarouselProduct1 = () => {
-    const imageUrl = './src/media/images/product1.jpg';
-  
-    return (
-      <div>
-      <img src={imageUrl} alt="product" />
+import product1Image from './media/images/product1.jpg';
 
-      </div>
-    );
-  };
-  
-  export default CarouselProduct1;
+const CarouselProduct1 = () => {
+  return (
+    <div>
+      <img src={product1Image} alt="product" />
+    </div>
+  );
+};
+
+export default CarouselProduct1;
